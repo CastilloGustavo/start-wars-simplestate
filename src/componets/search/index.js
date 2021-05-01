f86b60299da4e4ee53813c40f33b0d04eca479b0
Castillo Gustavo <castillogustavo22@gmail.com>
2021-05-01 20:23:23 +0100

    Se agrega componente de busqueda

---
+++ src/componets/search/index.js
@@ -0,0 +1,14 @@
+
+import './styles.scss';
+
+const search = (props) =>{
+
+    return(
+        <div className="div_main">
+            <input type='text'></input>
+            <button>B</button>
+        </div>
+    );
+};
+
+export default search;
